30d8680dd60e72d22317d86d902d1467d7edee6b
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2022-01-25 16:12:32 +1100

    Added Working Weather Component

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -4,27 +4,42 @@ import './WeatherItem.css'
 
 const WeatherItem = () => {
 
-  const [ weather, setWeather ] = useState()
+  const [ weather, setWeather ] = useState([])
+  const [ unit, setUnit ] = useState(true)
+  const [ unitConvert, setUnitConvert ] = useState('metric')
 
-  
-  const fetchWeather = async () => {
-    const axios = require('axios')
-    const city = 'sydney'
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_WEATHER_API_KEY}`
-    );
-
-    console.log(data)
+  const unitToggle = () => {
+    setUnit(!unit)
   }
+
+  const fetchWeather = () => {
+    // const axios = require('axios')
+    const city = 'Sydney'
+    axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+    ).then((response) => {
+      setWeather(response.data)
+      console.log(response.data)
+   })
+
+    }
 
  useEffect(() => {
     fetchWeather()
  }, []);
- 
 
   return ( 
-    <div>
-      <h2 className='weather-item'>Weather Item</h2>
+    <div className='weather-item'>
+      <span className='weather-name'>{weather ? weather.name : "no name"}</span> 
+      <span className='weather-temp'>
+        { unit ? (
+          weather.main ? Math.round(weather.main.temp - 273.15)+'°C' : '0')
+          : weather.main ? Math.round(weather.main.temp * 1.8 - 459.67)+'°F' : '0'
+        }
+        
+      </span>
+
+      <button onClick={unitToggle}>C | F</button>
     </div>
     )
 };
