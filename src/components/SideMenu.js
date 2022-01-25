c5ab51716b404a774e7fa552e956f46fc5aed1fe
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2022-01-25 19:24:29 +1100

    Updated Side Menu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Container } from '@mui/material';
+
 import './SideMenu.css'
+import { Box } from '@mui/system';
 
 const SideMenu = () => {
   return (
-  <div className='side-menu'>
-    <h1>Menu</h1>
-  </div>
+    <div className='side-menu'> 
+      <Container sx={{
+        bgcolor: 'background.paper',
+        height: '100%',
+        margin: '0px',
+        color: 'primary',
+
+      }}>
+        <Box sx={{
+          color: 'text.primary',
+          fontWeight: 'medium',
+          fontSize: '1rem',
+          padding: '10px',
+          paddingTop: '25px'
+        }}>
+        <div className='menu-item'>item</div>
+        <div className='menu-item'>item</div>
+        <div className='menu-item'>item</div>
+        <div className='menu-item'>item</div>
+        <div className='menu-item'>item</div>
+        
+        </Box>
+
+      </Container>
+    </div>
   )
 };
 
