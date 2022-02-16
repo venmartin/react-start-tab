fae9d5bbf11fdc0bdf78ba2c31b2b148816aecda
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2022-02-17 09:10:42 +1100

    Added props for weather modal

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import React, { useState } from 'react';
-import { Container } from '@mui/material';
-import './App.css';
-import MainSearch from './components/MainSearch';
-import SideMenu from './components/SideMenu';
-import Time from './components/Time';
-import WeatherItem from './components/WeatherItem';
+import React, { useState } from "react";
+import { Container } from "@mui/material";
+import "./App.css";
+import MainSearch from "./components/MainSearch";
+import SideMenu from "./components/SideMenu";
+import Time from "./components/Time";
+import WeatherItem from "./components/WeatherItem";
 
 function App() {
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false)
-  const [ isUnit, setIsUnit ] = useState(true)
-  const [ is24hour, setIs24Hour] = useState(true)
-  const [ degrees, setDegrees ] = useState('Celcius')
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isUnit, setIsUnit] = useState(true);
+  const [is24hour, setIs24Hour] = useState(true);
+  const [degrees, setDegrees] = useState("Celcius");
+
   // Control of the side menu
 
   const menuToggle = () => {
-    setIsMenuOpen(!isMenuOpen)
-      console.log(isMenuOpen)
-    
-  }
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
 
   const unitToggle = () => {
-    setIsUnit(!isUnit)
+    setIsUnit(!isUnit);
     if (isUnit === true) {
-      setDegrees('Fahrenheit')
+      setDegrees("Fahrenheit");
     } else {
-      setDegrees('Celcius')
-      
+      setDegrees("Celcius");
     }
-  }
+  };
 
   // End Side Menu
 
   // Control of the clock toggle for 12h to 24h.
   const toggleHours = () => {
-    if(is24hour === is24hour) {
-    setIs24Hour(!is24hour)
-    console.log(is24hour)
-   }
-  }
-
-
-
-
+    if (is24hour === is24hour) {
+      setIs24Hour(!is24hour);
+      console.log(is24hour);
+    }
+  };
 
   return (
     <div className="App">
-      <SideMenu menuToggle={menuToggle} isOpen={isMenuOpen} />
+      <SideMenu
+        menuToggle={menuToggle}
+        isOpen={isMenuOpen}
+        degrees={unitToggle}
+      />
       <MainSearch />
       <Time />
-      <WeatherItem menuOpen={isMenuOpen} unitToggle={unitToggle} isUnit={isUnit} degrees={degrees}/>
+      <WeatherItem
+        menuOpen={isMenuOpen}
+        unitToggle={unitToggle}
+        isUnit={isUnit}
+        degrees={degrees}
+      />
     </div>
   );
 }
