22e6054c74ccea6f0faf6928de012233e41de21b
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2022-02-17 09:14:32 +1100

    Created weather item for main display.

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -1,76 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './WeatherItem.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./WeatherItem.css";
+import WeatherModal from "./WeatherModal";
 
-const WeatherItem = ({ menuOpen, unitToggle, isUnit, degrees, weatherModal }) => {
-  const [ weather, setWeather ] = useState([])
-  const [ longitude, setLongitude ] = useState()
-  const [ latitude, setLatitude ] = useState()
-
-  // let lon = ''
-  // let lat = ''
+const WeatherItem = ({ menuOpen, unitToggle, isUnit }) => {
+  const [ weather, setWeather ] = useState([]);
+  const [ forecast, setForecast ] = useState([]);
+  const [ longitude, setLongitude ] = useState();
+  const [ latitude, setLatitude ] = useState();
+  const [ cityName, setCityName] = useState('')
 
   const getGeolocation = () => {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
         enableHighAccuracy: true,
-        timeout: 5000
-      })
+        timeout: 5000,
+      });
     } else {
-      console.log('Error: Not Supported');
+      console.log("Error: Not Supported");
     }
-  }
+  };
 
   const successCallback = (position) => {
-      const lon = position.coords.longitude
-      const lat = position.coords.latitude
-      console.log(lon)
-      console.log(lat)
-      setLongitude(lon)
-      setLatitude(lat)  
-      console.log(latitude)
-      console.log(longitude)
-      fetchWeather(lon, lat)
-    }
+    const lon = position.coords.longitude;
+    const lat = position.coords.latitude;
+    console.log(lon);
+    console.log(lat);
+    fetchWeather(lon, lat);
+  };
 
-    function errorCallback () {
-      console.log('Error: Location not allowed. Weather cannot be determined by location. Please use the search bar or enable location services.');
-     
-    }
+  function errorCallback() {
+    console.log(
+      "Error: Location not allowed. Weather cannot be determined by location. Please use the search bar or enable location services."
+    );
+  }
 
   const fetchWeather = (lon, lat) => {
     // const city = 'Sydney'
-    axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    ).then((response) => {
-      setWeather(response.data)
-      console.log(response.data)
-   }).catch(function (error) {
-     console.log(error)
-   })
- }
-
- 
- useEffect(() => {
-    getGeolocation()
-    // fetchWeather()
- }, []);
-
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      )
+      .then((response) => {
+        setWeather(response.data);
+        setCityName(response.data.name);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  console.log(cityName)
   
-  return ( 
-    <div className={ menuOpen ? 'weather-item weather-item-open' : 'weather-item'} onClick={weatherModal}>
-      <span className='weather-name'>{weather ? weather.name : "no name"}</span> 
-      <span className='weather-temp'>
-        { isUnit ? (
-          weather.main ? Math.round(weather.main.temp - 273.15)+'°C' : '0')
-          : weather.main ? Math.round(weather.main.temp * 1.8 - 459.67)+'°F' : '0'
-        }
-        
-      </span>
+  useEffect(() => {
+    getGeolocation();
+  }, []);
 
-      <button className='toggle-btn' onClick={unitToggle}>{degrees}</button>
+  return (
+    <div
+      className={menuOpen ? "weather-item weather-item-open" : "weather-item"}
+      // onClick={handleOpen}
+    >
+    <WeatherModal cityName={cityName} isUnit={isUnit} unitToggle={unitToggle}>
+      <span className="weather-name">{weather ? weather.name : "no name"}</span>
+      <span className="weather-temp">
+        {isUnit
+          ? weather.main
+            ? Math.round(weather.main.temp - 273.15) + "°C"
+            : "0"
+          : weather.main
+          ? Math.round(weather.main.temp * 1.8 - 459.67) + "°F"
+          : "0"}
+      </span>
+      </WeatherModal>
     </div>
-    )
+  );
 };
 
 export default WeatherItem;
