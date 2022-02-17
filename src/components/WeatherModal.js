59ec1f35c04443afbe7c0f17d76539af7a90c0a1
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2022-02-17 15:32:48 +1100

    Introduced Swiper to animate daily forecasts

--- src/components/WeatherModal.js
+++ src/components/WeatherModal.js
@@ -9,11 +9,22 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import CancelIcon from "@mui/icons-material/Cancel";
 import "./WeatherModal.css";
-import AirIcon from '@mui/icons-material/Air';
-import { ReactComponent as WiHumidity } from '../img/icons/WiHumidity.svg'
-import { ReactComponent as FiWind } from '../img/icons/FiWind.svg'
-import { ReactComponent as GiSunrise } from '../img/icons/GiSunrise.svg'
-import { ReactComponent as GiSunset } from '../img/icons/GiSunset.svg'
+import AirIcon from "@mui/icons-material/Air";
+import { ReactComponent as WiHumidity } from "../img/icons/WiHumidity.svg";
+import { ReactComponent as FiWind } from "../img/icons/FiWind.svg";
+import { ReactComponent as GiSunrise } from "../img/icons/GiSunrise.svg";
+import { ReactComponent as GiSunset } from "../img/icons/GiSunset.svg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import "swiper/css/bundle";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { styled } from "@mui/material/styles";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import Stack from "@mui/material/Stack";
 
 const style = {
   position: "absolute",
@@ -28,6 +39,50 @@ const style = {
   boxShadow: 24,
   p: 4,
 };
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#00ffff75" : " #00ffff90",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 const WeatherModal = ({
   isUnit,
@@ -46,19 +101,18 @@ const WeatherModal = ({
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
     setOpen(true);
-    fetchForecast(cityName)
+    fetchForecast(cityName);
   };
   const handleClose = () => setOpen(false);
-  const [ latitude, setLatitude ] = useState()
-  const [ longitude, setLongitude ]= useState()
-  const [ weather, setWeather] = useState([]);
-  const [ forecast, setForecast] = useState({});
-  const [ searchCity, setSearchCity] = useState('');
-  const [ iconImgCurrent, setIconImgCurrent ] = useState()
-  const [ iconImgDaily, setIconImgDaily ] = useState()
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [weather, setWeather] = useState([]);
+  const [forecast, setForecast] = useState({});
+  const [searchCity, setSearchCity] = useState("");
+  const [iconImgCurrent, setIconImgCurrent] = useState();
+  const [iconImgDaily, setIconImgDaily] = useState();
   // const [ isUnit, setIsUnit ] = useState(true)
-  
-  
+
   const fetchForecast = (city) => {
     axios
       .get(
@@ -67,9 +121,9 @@ const WeatherModal = ({
       )
       .then((response) => {
         setWeather(response.data);
-        const dataLat = response.data.coord.lat
-        const dataLon = response.data.coord.lat
-        weeklyForecast(dataLat, dataLon)
+        const dataLat = response.data.coord.lat;
+        const dataLon = response.data.coord.lat;
+        weeklyForecast(dataLat, dataLon);
         console.log(weather);
       })
       .catch(function (error) {
@@ -88,50 +142,43 @@ const WeatherModal = ({
   // }
 
   const submitCity = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
       )
       .then((response) => {
         setWeather(response.data);
-        let dataLat = response.data.coord.lat
-        let dataLon = response.data.coord.lon
-        weeklyForecast(dataLat, dataLon)
-
+        let dataLat = response.data.coord.lat;
+        let dataLon = response.data.coord.lon;
+        weeklyForecast(dataLat, dataLon);
       })
       .catch(function (error) {
         console.log(error);
       });
-      
-  }
-  
-
+  };
 
   const weeklyForecast = (lat, lon) => {
     axios
-    .get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    )
-    .then((response) => {
-      setForecast(response.data);
-      console.log(forecast);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-  }
-  let daily = ''
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+      )
+      .then((response) => {
+        setForecast(response.data);
+        console.log(forecast);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  let daily = "";
   const today = (day) => {
-    let thisDay = new Date(day)
+    let thisDay = new Date(day);
     // let daily = thisDay.getDay()
-  }
+  };
 
-  useEffect(() => {
-    
-  }, []);
-    
+  useEffect(() => {}, []);
+
   return (
     <div>
       <div className="btn-modal" onClick={handleOpen}>
@@ -155,62 +202,174 @@ const WeatherModal = ({
             </div>
             <div className="forecast-container">
               <div className="weather-search">
-                <form onSubmit={submitCity}>
-                  <input type="text" onChange={handleChange}></input>
-                  <div className='toggle-degrees' onClick={unitToggle}>TOGGLE</div>
+                <form className="search-form-modal" onSubmit={submitCity}>
+                  <div className="toggle-degrees">
+                    <FormGroup>
+                      <Stack direction="row" spacing={1} alignItems="center">
+                        <AntSwitch
+                          onClick={unitToggle}
+                          defaultChecked
+                          inputProps={{ "aria-label": "ant design" }}
+                        />
+                      </Stack>
+                    </FormGroup>
+                        {isUnit ? (
+                          <span>Metric</span>
+                        ) : (
+                          <span>Imperial</span>
+                        )}
+                  </div>
+                  <input
+                    className="search-input"
+                    type="text"
+                    onChange={handleChange}
+                    placeholder="Search by City Name"
+                  ></input>
                 </form>
               </div>
               <div className="detailed-current">
-                <div className='current-main'>
-                  <div className='current-top'>
-                    <div className='current-temp'>
-                      { isUnit ?
-                        forecast.current ? <span>{Math.round(forecast.current.temp - 273.15) + "°C"}</span> : "35 °C" 
-                        :
-                        forecast.current ? <span>{Math.round(forecast.current.temp * 1.8 - 459.67) + "°F"}</span> : "85 °F"
-                        }
+                <div className="current-main">
+                  <div className="current-top">
+                    <div className="current-temp">
+                      {isUnit ? (
+                        forecast.current ? (
+                          <span>
+                            {Math.round(forecast.current.temp - 273.15) + "°C"}
+                          </span>
+                        ) : (
+                          "35 °C"
+                        )
+                      ) : forecast.current ? (
+                        <span>
+                          {Math.round(forecast.current.temp * 1.8 - 459.67) +
+                            "°F"}
+                        </span>
+                      ) : (
+                        "85 °F"
+                      )}
                     </div>
-                    <div className='current-icon'>
-                        {forecast.current ? <img src={`http://openweathermap.org/img/wn/${forecast.current.weather[0].icon}@2x.png`}></img> : <img src="http://openweathermap.org/img/wn/01d@2x.png"></img> }
-                        {forecast.current ? <span className='weather-description'>{forecast.current.weather[0].description}</span> : 'Clear'}
+                    <div className="current-icon">
+                      {forecast.current ? (
+                        <img
+                          src={`http://openweathermap.org/img/wn/${forecast.current.weather[0].icon}@2x.png`}
+                        ></img>
+                      ) : (
+                        <img src="http://openweathermap.org/img/wn/01d@2x.png"></img>
+                      )}
+                      {forecast.current ? (
+                        <span className="weather-description">
+                          {forecast.current.weather[0].description}
+                        </span>
+                      ) : (
+                        "Clear"
+                      )}
                     </div>
                   </div>
-                  <div className='vert-divider'></div>
-                  <div className='current-details'>
-                  { forecast.current ? <span className='current-desc'><GiSunrise /> {new Date(forecast.current.sunrise * 1000).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})}</span>
-                        : '06:00 AM'
-                        }                        
-                  { forecast.current ? <span className='current-desc'><GiSunset /> {new Date(forecast.current.sunset * 1000).toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})}</span>
-                        : '06:00 AM'
-                        }                        
-                  { forecast.current ? <span className='current-desc'><WiHumidity/> {forecast.current.humidity}%</span>
-                        : '80%'
-                        }                        
-                  { forecast.current ? <span className='current-desc'><FiWind /> {forecast.current.wind_speed}km/h</span>
-                        : '80%'
-                        }                        
+                  <div className="vert-divider"></div>
+                  <div className="current-details">
+                    {forecast.current ? (
+                      <span className="current-desc">
+                        <GiSunrise />{" "}
+                        {new Date(
+                          forecast.current.sunrise * 1000
+                        ).toLocaleTimeString(navigator.language, {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    ) : (
+                      "06:00 AM"
+                    )}
+                    {forecast.current ? (
+                      <span className="current-desc">
+                        <GiSunset />{" "}
+                        {new Date(
+                          forecast.current.sunset * 1000
+                        ).toLocaleTimeString(navigator.language, {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    ) : (
+                      "06:00 AM"
+                    )}
+                    {forecast.current ? (
+                      <span className="current-desc">
+                        <WiHumidity /> {forecast.current.humidity}%
+                      </span>
+                    ) : (
+                      "80%"
+                    )}
+                    {forecast.current ? (
+                      <span className="current-desc">
+                        <FiWind /> {forecast.current.wind_speed}km/h
+                      </span>
+                    ) : (
+                      "80%"
+                    )}
                   </div>
-
                 </div>
-
               </div>
               <div className="current-city">SYDNEY</div>
-
-              <div className="weekly-forecast">
-                { forecast.daily && forecast.daily.map((item, i) =>
-                  <ForecastItem
-                    key={item.dt}
-                    day={item.dt}
-                    temp={item.temp.day}
-                    min_temp={item.temp.min}
-                    max_temp={item.temp.max}
-                    feels_like={item.feels_like.day}
-                    icon={item.weather[0].icon}
-                    description={item.weather[0].description}
-                    wind_speed={item.wind_speed}
-                  />
-                )}
-              </div>
+              <Swiper
+                // install Swiper modules
+                modules={[Navigation, Scrollbar, A11y]}
+                spaceBetween={10}
+                slidesPerView={5}
+                navigation={{
+                  color: "red",
+                }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log("slide change")}
+              >
+                {/* <div className="weekly-forecast"> */}
+                {forecast.daily &&
+                  forecast.daily.map((item, i) => (
+                    <SwiperSlide key={item.sunrise}>
+                      <ForecastItem
+                        key={item.dt}
+                        day={item.dt}
+                        temp={
+                          isUnit
+                            ? `${Math.round(item.temp.day - 273.15) + "°C"}`
+                            : `${
+                                Math.round(item.temp.day * 1.8 - 459.67) + "°F"
+                              }`
+                        }
+                        min_temp={
+                          isUnit
+                            ? `${Math.round(item.temp.min - 273.15) + "°C"}`
+                            : `${
+                                Math.round(item.temp.min * 1.8 - 459.67) + "°F"
+                              }`
+                        }
+                        max_temp={
+                          isUnit
+                            ? `${Math.round(item.temp.max - 273.15) + "°C"}`
+                            : `${
+                                Math.round(item.temp.max * 1.8 - 459.67) + "°F"
+                              }`
+                        }
+                        humidity={item.humidity}
+                        feels_like={
+                          isUnit
+                            ? `${
+                                Math.round(item.feels_like.day - 273.15) + "°C"
+                              }`
+                            : `${
+                                Math.round(item.feels_like.day * 1.8 - 459.67) +
+                                "°F"
+                              }`
+                        }
+                        icon={item.weather[0].icon}
+                        description={item.weather[0].description}
+                        wind_speed={isUnit ? Math.round(item.wind_speed.toFixed(2)) + ' kmph' : Math.round(item.wind_speed.toFixed(2) * 0.62137119) + ' mph'}
+                      />
+                    </SwiperSlide>
+                  ))}
+                {/* </div> */}
+              </Swiper>
             </div>
           </Box>
         </Fade>
@@ -220,3 +379,4 @@ const WeatherModal = ({
 };
 
 export default WeatherModal;
+
