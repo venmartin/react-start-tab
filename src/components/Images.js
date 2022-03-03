35c999588258cf4c590444a37183d0d71194bb12
Adam Martinovic <adam.martinovic@protonmail.com>
2022-03-03 15:42:26 +1100

    Created a image array component for bg selector

---
+++ src/components/Images.js
@@ -0,0 +1,45 @@
+import bg1 from "../img/bg/bg_beach1.jpg"
+import bg2 from "../img/bg/bg_city1.jpg"
+import bg3 from "../img/bg/bg_city2.jpg"
+import bg4 from "../img/bg/bg_city3.jpg"
+import bg5 from "../img/bg/bg_forest1.jpg"
+import bg6 from "../img/bg/bg_indoorforest.jpg"
+import bg8 from "../img/bg/bg_mountain1.jpg"
+import bg9 from "../img/bg/bg_mountain2.jpg"
+import bg10 from "../img/bg/bg_mountain3.jpg"
+import bg11 from "../img/bg/bg_nature1.jpg"
+import bg12 from "../img/bg/bg_road1.jpg"
+import bg13 from "../img/bg/bg_snow1.jpg"
+import bg14 from "../img/bg/bg_waterfall.jpg"
+import bg15 from "../img/bg/cyberpunk1.jpg"
+import bg16 from "../img/bg/cyberpunk2.jpg"
+import bg17 from "../img/bg/default_bg.jpg"
+import bg18 from "../img/bg/neon1.jpg"
+import bg19 from "../img/bg/night.jpg"
+import bg20 from "../img/bg/night_city1.jpg"
+import bg21 from "../img/bg/night_city2.jpg"
+import bg22 from "../img/bg/night_city3.jpg"
+
+export default [
+bg1, 
+bg2, 
+bg3, 
+bg4, 
+bg5, 
+bg6, 
+bg8, 
+bg9, 
+bg10,
+bg11,
+bg12,
+bg13,
+bg14,
+bg15,
+bg16,
+bg17,
+bg18,
+bg19,
+bg20,
+bg21,
+bg22
+]
